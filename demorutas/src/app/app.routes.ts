59686ba731c5c68faf806ec665258ed25e092b06
Jff-107 <Jeffpachecosar@gmail.com>
2026-06-30 15:08:53 +0000

    Semana 14 get post??

--- demorutas/src/app/app.routes.ts
+++ demorutas/src/app/app.routes.ts
@@ -4,8 +4,14 @@ import { FormReactivo } from './form-reactivo/form-reactivo';
 import { Detalle1 } from './detalle1/detalle1';
 import { NoEncontrado } from './no-encontrado/no-encontrado';
 import { Detalle2 } from './detalle2/detalle2';
+import { FormUsuario } from './form-usuario/form-usuario';
 
 export const routes: Routes = [
+        {
+        path: 'formusuarios',
+        component: FormUsuario,
+        title: 'Usuarios con service',
+        },
         {
         path: 'formreactivo',
         component: FormReactivo,
