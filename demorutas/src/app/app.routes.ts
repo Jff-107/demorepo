55b91881e426b4686e0c40ed2100d0aab6308bfd
Jff-107 <Jeffpachecosar@gmail.com>
2026-06-30 13:39:31 +0000

    Actualizacion de formularios

---
+++ demorutas/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { FormPlantilla } from './form-plantilla/form-plantilla';
+import { FormReactivo } from './form-reactivo/form-reactivo';
+import { Detalle1 } from './detalle1/detalle1';
+import { NoEncontrado } from './no-encontrado/no-encontrado';
+import { Detalle2 } from './detalle2/detalle2';
+
+export const routes: Routes = [
+        {
+        path: 'formreactivo',
+        component: FormReactivo,
+        title: 'Formulario reactivo',
+        },
+        {
+        path: 'formplantilla',
+        component: FormPlantilla,
+        title: 'Formulario basado en plantilla',
+        },    
+        {
+        path: 'detalle1',
+        component: Detalle1,
+        title: 'Detalle 1',
+        },
+        
+        {
+        path: 'detalle2/:miparam',
+        component: Detalle2,
+        title: 'Detalle 2',
+        },
+
+        {
+    path: '**',
+    component: NoEncontrado,
+    title: 'Pagina no encontrada',
+    }
+];
